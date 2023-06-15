41dadf1702d6d77a7247a7c10d99e86cd619b83e
kirubelastr <132957975+kirubelastr@users.noreply.github.com>
2023-06-15 13:59:19 +0300

    a

---
+++ filllogin.php
@@ -0,0 +1,30 @@
+<?php
+// Start a session
+session_start();
+
+// Connect to the database
+require_once "connection.php";
+
+// Check if the form has been submitted
+if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["role"])) {
+  // Get the email, password, and role from the form
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $role = $_POST["role"];
+
+  // Check if the email, password, and role exist in the login table
+  $query = "SELECT * FROM login WHERE email='$email' AND password='$password' AND role='$role'";
+  $result = mysqli_query($db, $query);
+
+  if (mysqli_num_rows($result) == 1) {
+    // Login successful
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION["user_type"] = $row["user_type"];
+    $_SESSION["user_id"] = $row["id"];
+    header("Location: home.php");
+  } else {
+    // Login failed
+    echo "Invalid email, password, or role";
+  }
+}
+?>
