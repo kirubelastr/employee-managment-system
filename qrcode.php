8d63244de02f357c3b58d187bc2f4b0f5ee7a4a5
kirubelastr <132957975+kirubelastr@users.noreply.github.com>
2023-06-23 09:48:26 +0300

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -81,7 +81,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     ));
 }
 
-// Search for a specific employee/manager ID and display the corresponding QR code in a popup when clicked
+// Search for a specific employee/manager ID and display the corresponding QR code right under the form
 if (isset($_POST["searchType"]) && isset($_POST["searchID"])) {
     // Get search type and ID from form data
     $searchType = $_POST["searchType"];
@@ -91,7 +91,7 @@ if (isset($_POST["searchType"]) && isset($_POST["searchID"])) {
     foreach ($qrcodes as &$qrcode) {
         if (($searchType == "employee" && isset($qrcode["employeeID"]) && strval($qrcode["employeeID"]) == strval($searchID)) ||
             ($searchType == "manager" && isset($qrcode["managerID"]) && strval($qrcode["managerID"]) == strval($searchID))) {
-            // Display the corresponding QR code in a popup when clicked
+            // Display the corresponding QR code right under the form
             echo '<img src="data:image/png;base64,' . htmlspecialchars($qrcode["qrimage"]) . '" onclick="showQRCode(this)"/>';
             // Display a print button
             echo '<button onclick="printQRCode(this)">Print</button>';
@@ -123,8 +123,223 @@ if (isset($_POST["searchType"]) && isset($_POST["searchID"])) {
             printWindow.close();
         }
     </script>
+<style>   body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: stretch;
+}
+.page-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: stretch;
+    }
+
+    .content-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: stretch;
+    }
+.sidebar {
+  width: 200px;
+  height: 100vh;
+  background-color: #f0f0f0;
+  padding: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.sidebar h3 {
+  margin-top: 0;
+}
+
+.sidebar a {
+  display: block;
+  margin-bottom: 10px;
+  padding: 10px;
+  width: 100%;
+  text-align: center;
+  text-decoration: none;
+  color: #333;
+  border-left: 5px solid transparent;
+}
+
+.sidebar a.active,
+.sidebar a:hover {
+  background-color: #ddd;
+  border-left-color: red;
+}
+.rightofsidebar{
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: baseline;
+}
+.container {
+ max-width: auto;
+ height: auto;
+ margin: 10px;
+ padding: 20px;
+ background-color: #fff;
+ box-shadow: 0 0 10px rgba(0,0,0,0.2);
+  }
+h3 {
+  margin-top: 0;
+ }
+ hr {
+ margin: 20px 0;
+ border: none;
+ border-top: 1px solid #ccc;
+}
+
+.employee-info {
+            text-align: center;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #000000;
+            background-color: #FFFFFF;
+         }
+         table {
+        border-collapse: collapse;
+        width: 100%;
+        }
+        th, td {
+            text-align: left;
+            padding: 8px;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+.form-container {
+  flex: 1;
+  padding: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+}
+
+.form-container h2 {
+    margin-top:0; 
+    margin-bottom :10px; 
+}
+
+.form-section {
+    margin-bottom :5px; 
+    border :1px solid #ccc; 
+    border-radius :4px; 
+    padding :10px; 
+    box-sizing :border-box; 
+}
+.form-section2{
+    height :350px; 
+    margin-bottom :5px; 
+    border :1px solid #ccc; 
+    border-radius :4px; 
+    padding :10px; 
+    box-sizing :border-box; 
+    
+}
+
+.form-section h3 {
+    margin-top :0; 
+    margin-bottom :5px; 
+}
+form {
+    display:block; 
+    flex-wrap :wrap; 
+    justify-content:left; 
+    align-items:center; 
+}
+
+label {
+    display:inline-block; 
+    width :140px; 
+    text-align:right; 
+    margin-right :20px; 
+}
+    
+input[type="submit"] {
+      background-color:#09f ; 
+      color:#fff ; 
+      border:none ; 
+      border-radius :4px ; 
+      padding :10px ; 
+      cursor:pointer ; 
+      font-size :16px ; 
+      margin-top :10px ;  
+}
+input,
+select,
+textarea {
+      position :inherits ;  
+      flex :1 ;  
+      padding :10px ;  
+      border :1px solid #ccc ;  
+      border-radius :4px ;  
+      box-sizing:border-box ;  
+      margin-bottom :10px ;  
+      font-size :16px ;  
+}
+
+input[type="file"] {
+      padding :10 ;
+}
+
+input:focus,
+select:focus,
+textarea:focus {
+      outline:none ;
+      border-color:#09f ;
+}
+
+select {
+      width :inherit ;
+      padding-left: 20px;
+      padding-right: 40px;
+}
+
+textarea {
+      height :auto ;
+}
+
+input[type="submit"]:hover {
+      background-color:#0077cc ;
+}
+
+  </style>
 </head>
 <body>
+<div class="page-container">
+  <?php include 'header.php'; ?>
+
+  <div class="content-container">
+  <div class="sidebar">
+    <h3>Sidebar</h3>
+    <a href="department_and_position.php">department and position</a>
+    <a href="aproveleave.php">aprove leave</a>
+    <a href="createusers.php">createusers</a>
+    <a href="employee.php">add employee</a>
+    <a href="manager.php">add manage</a>
+    <a class="active"href="deductionandallowance.php">deduction and allowance</a>
+    <a href="qrcode.php">qrcode</a>
+  </div>
+  <div class="rightofsidebar">
+<div class="container">
     <!-- Search form -->
     <form method="post">
         <label for="searchType">Search Type:</label>
@@ -136,6 +351,27 @@ if (isset($_POST["searchType"]) && isset($_POST["searchID"])) {
         <input type="text" name="searchID" id="searchID"><br><br>
         <input type="submit" value="Search">
     </form>
+
+    <?php
+    // Display the searched QR code right under the form
+    if (isset($_POST["searchType"]) && isset($_POST["searchID"])) {
+        // Get search type and ID from form data
+        $searchType = $_POST["searchType"];
+        $searchID = $_POST["searchID"];
+
+        // Search for the specified ID in the qrcodes array
+        foreach ($qrcodes as &$qrcode) {
+            if (($searchType == "employee" && isset($qrcode["employeeID"]) && strval($qrcode["employeeID"]) == strval($searchID)) ||
+                ($searchType == "manager" && isset($qrcode["managerID"]) && strval($qrcode["managerID"]) == strval($searchID))) {
+                // Display the corresponding QR code right under the form
+                echo '<img src="data:image/png;base64,' . htmlspecialchars($qrcode["qrimage"]) . '" onclick="showQRCode(this)"/>';
+                // Display a print button
+                echo '<button onclick="printQRCode(this)">Print</button>';
+                break;
+            }
+        }
+    }
+    ?>
 
     <!-- QR code images section -->
     <div class="qr-images-container">
