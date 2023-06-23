893c58cda516f8c4df865835bcadb4376cf5c039
kirubelastr <132957975+kirubelastr@users.noreply.github.com>
2023-06-23 21:26:57 +0300

    asd

--- branch/aprovebranchleave.php
+++ branch/aprovebranchleave.php
@@ -1,28 +1,88 @@
-<h3>Sidebar</h3>
-<a href="managerdashboard.php">Home</a>
-<a href="managerleave.php">leave</a>
-<a href="managerattendance.php">attendance</a>
-<a class="active" href="managerdetails.php">details
-    <ul id="sub-menu">
-        <li><a href="#">manage employees in this branch</a></li>
-    </ul>
-</a>
+<?php 
+require_once "connection.php";
 
-<!-- Include the jQuery library -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+// get pending leave requests for employees in the same branch as the branch manager
+$sql = "SELECT el.*, IF(el.employeeID IS NOT NULL, e.yearlyvacationdays, m.yearlyvacationdays) AS vacationdays 
+        FROM employee_leave el
+        LEFT JOIN employee e ON el.employeeID = e.employeeID
+        LEFT JOIN manager m ON el.managerID = m.managerID
+        JOIN branch b ON e.branchID = b.branchID
+        WHERE el.status = 'pending' AND b.managerID = " . $_SESSION['user_type'];
+$result = $conn->query($sql);
 
-<!-- Add the JavaScript code to toggle the sub-menu -->
-<script>
-$(document).ready(function() {
-    $('.active').click(function() {
-        $('#sub-menu').toggle();
-    });
-});
-</script>
+if ($result->num_rows > 0) {
+    // output pending leave requests
+    echo "<table>";
+    echo "<tr><th>Leave ID</th><th>Employee ID</th><th>Manager ID</th><th>Date</th><th>Leave Type</th><th>Start Date</th><th>End Date</th><th>Status</th><th>Vacation Days</th><th>Actions</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        // get employee's or manager's yearly vacation days based on non-null value
+        $vacationdays = $row["vacationdays"];
 
-<!-- Add the CSS code to hide the sub-menu by default -->
-<style>
-#sub-menu {
-    display: none;
+        echo "<tr>";
+        echo "<td>" . $row["leaveID"] . "</td>";
+        echo "<td>" . $row["employeeID"] . "</td>";
+        echo "<td>" . $row["managerID"] . "</td>";
+        echo "<td>" . $row["date"] . "</td>";
+        echo "<td>" . $row["leavetype"] . "</td>";
+        echo "<td>" . $row["startdate"] . "</td>";
+        echo "<td>" . $row["enddate"] . "</td>";
+        echo "<td>" . $row["status"] . "</td>";
+        echo "<td>" . $vacationdays . "</td>";
+        echo "<td>";
+        // show approve and reject buttons
+        echo '<form method="post" action="update.php">';
+        echo '<input type="hidden" name="leaveID" value="' . $row["leaveID"] . '">';
+        echo '<input type="submit" name="approve" value="Approve">';
+        echo '<input style="background-color: red;" type="submit" name="reject" value="Reject">';
+        echo '</form>';
+        echo "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No pending leave requests";
 }
-</style>
+echo '<div class="container">';
+
+// get approved/denied leave requests for employees in the same branch as the branch manager
+$sql = "SELECT el.* FROM employee_leave el
+        JOIN employee e ON el.employeeID = e.employeeID
+        JOIN branch b ON e.branchID = b.branchID
+        WHERE el.status != 'pending' AND b.managerID = " . $_SESSION['user_type'];
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output approved/denied leave requests
+    echo "<table>";
+    echo "<tr><th>Leave ID</th><th>Employee ID</th><th>Manager ID</th><th>Date</th><th>Leave Type</th><th>Start Date</th><th>End Date</th><th>Status</th></tr>";
+    while($row = $result->fetch_assoc()) {
+      
+        // set background color based on status
+        if ($row['status'] == 'pending') {
+          $bg_color = 'blue';
+      } elseif ($row['status'] == 'approved') {
+          $bg_color = 'green';
+      } else {
+          $bg_color = 'red';
+      }
+      echo "<tr>";
+      echo "<td>" . $row["leaveID"] . "</td>";
+      echo "<td>" . $row["employeeID"] . "</td>";
+      echo "<td>" . $row["managerID"] . "</td>";
+      echo "<td>" . $row["date"] . "</td>";
+      echo "<td>" . $row["leavetype"] . "</td>";
+      echo "<td>" . $row["startdate"] . "</td>";
+      echo "<td>" . $row["enddate"] . "</td>";
+      echo "<td style='background-color: " . $bg_color . ";'>" . $row["status"] . "</td>";
+      echo "<td></td>";
+      echo "</tr>";
+  }
+  echo "</table>";
+} else {
+  echo "No approved/denied leave requests";
+}
+?>
+</div>
+</div>
+</body>
+</html>
